f29c8ba1349ae9ef26da11f9211a66d091d57e04
RonaldGomez <ronald.gomez@vallenova.pe>
2014-12-14 00:37:26 -0500

    Se agrega configura la pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'estructura_paginas/Inicio'
-
+  #get 'estructura_paginas/Inicio'
+  root 'estructura_paginas#Inicio'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
