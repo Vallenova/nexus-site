c38e583d72753be761a9c11579d1770100f15813
Dante Alferez Condori <dante.alferez@vallenova.pe>
2014-12-31 00:14:46 -0500

    Se resetea la bbdd

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222231755) do
+ActiveRecord::Schema.define(version: 20141228163445) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "nombre"
     t.string   "estado"
     t.string   "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "email"
+    t.string   "nombres"
+    t.string   "apellidos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
