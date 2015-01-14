1c837d293be0e9135fb300f32ef75b2a737a7748
RonaldGomez <ronald.gomez@vallenova.pe>
2015-01-14 15:45:52 -0500

    Se agrega asignacion en la pagina de inicio

--- app/controllers/estructura_paginas_controller.rb
+++ app/controllers/estructura_paginas_controller.rb
@@ -1,10 +1,12 @@
 class EstructuraPaginasController < ApplicationController
   
   def inicio
+  	@area_diplomados = AreaDiplomado.where(estado: 'A').order(:id)
   	@areas=Area.where(estado: 'A').order(:id)
   end
 
   def acerca
+  	@area_diplomados = AreaDiplomado.where(estado: 'A').order(:id)
   	@areas=Area.where(estado: 'A').order(:id)
   end
 
